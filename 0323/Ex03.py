19a7ba5e37c9249f5af3674bc568105ce528946e
WatsonFan <30586391+watosnfan@users.noreply.github.com>
2023-03-23 20:47:43 +0800

    Add files via upload

---
+++ 0323/Ex03.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 23 19:51:05 2023
+
+@author: USER
+"""
+
+
+scores = [100,98,88,66,90]
+
+items = [99,"一中",3.14,True]
+
+print(items[1])
+print(scores[0])
+
+number = [1,2,3,4,5,6,7,8,9,0]
+
+print(number[-1])
+print(number[-2])
+print(number[2:5])
+print(number[:5])
+print(number[5:])
+print(number[1:9:2])
+print(number[-1::-1])
+
+print()
+
+
+
+for i in range(len(number)):
+    print(number[i])
+    
+print()    
+
+
+for it in number:
+    print(it)
+
+
+
+
+
+
+
---
+++ 0323/ex01.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("姓名：%s，零用錢有：%d" % ("Bill",1000))
+print("利率：%f" % (3.14))
+print("利率：%.2f" % (3.1415926))
+
+print("姓名：%10s。" % ("Bill"))
+print("姓名：%-10s。" % ("Bill"))
+print("存款：%6d" %(1000))
+print("存款：%5.1f" % (3.14))
+     
